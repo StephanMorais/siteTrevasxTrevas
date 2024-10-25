e93adf8fe20d27f9c06a757a76b03d7dc5e528d7
renatosacomano <renatosacomanodacruz@gmail.com>
2024-10-25 16:47:45 -0300

    batman

---
+++ src/main/java/com/trevascontratrevas/txtSiteServer/TxtSiteServerApplication.java
@@ -0,0 +1,13 @@
+package com.trevascontratrevas.txtSiteServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TxtSiteServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TxtSiteServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trevascontratrevas/txtSiteServer/TxtSiteServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trevascontratrevas.txtSiteServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TxtSiteServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
