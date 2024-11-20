a106ea935b7ecb516a4dcc104d6d633efaa1dc22
renatosacomano <renatosacomanodacruz@gmail.com>
2024-11-20 17:38:17 -0300

    Login Teste Page

--- src/main/java/com/trevascontratrevas/txtSiteServer/Controller/ttController.java
+++ src/main/java/com/trevascontratrevas/txtSiteServer/Controller/ttController.java
@@ -1,6 +1,7 @@
 package com.trevascontratrevas.txtSiteServer.Controller;
 
 import com.trevascontratrevas.txtSiteServer.Repository.*;
+import com.trevascontratrevas.txtSiteServer.models.Usuario;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
-public class ttController {
+public class ttController{
 	private RepoUsuario repoUsuario;
 	
 	@RequestMapping(value="/",method=RequestMethod.GET)
@@ -19,4 +20,13 @@ public class ttController {
 		return "index.html";
 	}
 	
+	@RequestMapping(value="/cadastro",method=RequestMethod.POST)
+	public String cadastro(Usuario usr,BindingResult result, RedirectAttributes attributes) {
+	 if(result.hasErrors()) {
+		 attributes.addFlashAttribute("mensagem","falhou");
+		 return "index.html";
+	 }
+	 
+	 return "index.html";
+	}
 }
